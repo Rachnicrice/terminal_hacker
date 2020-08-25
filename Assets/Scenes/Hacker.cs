05363ecbc284de1677a4dc91e09527283de7b323
Rachael <Rachnicrice@gmail.com>
2020-08-25 09:31:15 -0700

    My first unity project!

---
+++ Assets/Scenes/Hacker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        print("Hello Console");
+        ShowMainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Displays game entry screen
+    void ShowMainMenu () {
+        Terminal.ClearScreen();
+        string greeting = "Hello Rachael";
+        Terminal.WriteLine(greeting);
+
+        //Terminal.WriteLine("Welcome Captain. Infiltrate the");
+        Terminal.WriteLine("following federation systems.");
+        Terminal.WriteLine("Refuse and we will detonate the red");
+        Terminal.WriteLine("matter at your planet's core.");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 for the Kobayashi Maru");
+        Terminal.WriteLine("Press 2 for the USS Enterprise");
+        Terminal.WriteLine("Press 3 for the Starfleet Mainframe");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Resistance is futile.");
+        Terminal.WriteLine("Choose wisely: ");
+    }
+}
